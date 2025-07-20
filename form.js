d9e7ed8b2ff01415a36f136ce458e19c84cd552e
Moshe Ben-Ami <mobenami@synamedia.com>
2025-07-20 16:49:27 +0300

    adding tikshuv

--- form.js
+++ form.js
@@ -22,6 +22,48 @@ fetch(`forms/${formName}.json`)
       $("#surveyContainer").html(""); // נקה
       $("#surveyContainer").Survey({ model: displaySurvey });
 
+      if (formName === "tikshuv") {
+        const tikshuvData = JSON.parse(JSON.stringify(data));
+        // "openedList": [
+        //     {
+        //       "item": "פריט",
+        //       "quantity": 4
+        //     }
+        //   ],
+        if (tikshuvData.openedList) {
+          tikshuvData.openedList.forEach(element => {
+            tikshuvData[element.item] = element.quantity;
+          });
+          delete tikshuvData.openedList;
+        }
+
+        // "closedList": {
+        //     "שולחן": {
+        //       "quantity": 4
+        //     }
+        //   },          
+        if (tikshuvData.closedList) {
+            for (const [key, value] of Object.entries(tikshuvData.closedList)) {
+                tikshuvData[key] = value.quantity;
+            }
+            delete tikshuvData.closedList;
+        }
+
+        console.log("before" + JSON.stringify(data, null, 2));
+        console.log("after" + JSON.stringify(tikshuvData));
+
+        fetch("https://script.google.com/macros/s/AKfycbylh5l7ev_a3NC_pZaDAEEpJPoQM-FPqrtnpjMM--FpzNSJKh-Y3cNMckPi3OxQweYw8g/exec", {
+            method: "POST",
+            body: JSON.stringify(tikshuvData),
+            headers: {
+              "Content-Type": "application/json",
+            }
+          })
+//           .then(response => response.text())
+//            .then(result => console.log("Saved:", result))
+//              .catch(error => console.error("Error:", error));
+      }
+
       const phoneNumber = data.phone?.replace(/[^0-9]/g, "").replace(/^0+/, ''); // מסנן רק ספרות
       if (phoneNumber) {
           window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent("ההשאלה נרשמה")}`, "_blank");
